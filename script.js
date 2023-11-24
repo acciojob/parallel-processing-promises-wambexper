299c98f1e8879c7ccafc2a8cc4b874bf3344bb23
PETER WAMBEDDE <wambexpeter@gmail.com>
2023-11-24 12:24:36 +0300

    created a submission

--- script.js
+++ script.js
@@ -1,29 +1,43 @@
 //your JS code here. If required.
-const imageUrls = [
-  {url: 'https://via.placeholder.com/150', alt: 'Image 1'},
-  {url: 'https://via.placeholder.com/150', alt: 'Image 2'},
-  {url: 'https://via.placeholder.com/150', alt: 'Image 3'},
-  {url: 'https://via.placeholder.com/150', alt: 'Image 4'},
-  {url: 'https://via.placeholder.com/150', alt: 'Image 5'},
+const output = document.getElementById("output");
+const btn = document.getElementById("download-images-button");
+
+const images = [
+  { url: "https://picsum.photos/id/237/200/300" },
+  { url: "https://picsum.photos/id/238/200/300" },
+  { url: "https://picsum.photos/id/239/200/300" },
 ];
 
-const downloadImages = async () => {
-  try {
-    const promises = imageUrls.map(async (image) => {
-      const response = await fetch(image.url);
-      if (!response.ok) {
-        throw new Error(`Failed to load image's URL: ${image.url}`);
-      }
-      const blob = await response.blob();
-      const objectUrl = URL.createObjectURL(blob);
-      return {objectUrl, alt: image.alt};
-    });
-    const imageObjects = await Promise.all(promises);
-    const imagesHtml = imageObjects.map((image) => `<img src="${image.objectUrl}" alt="${image.alt}" />`).join('');
-    document.getElementById('output').innerHTML = imagesHtml;
-  } catch (error) {
-    console.error(error);
-  }
-};
+let file1 = new Promise((resolve)=>{
+	setTimeout(()=>{
+		resolve(images[0].url);
+	}.2000)
+})
+let file2 = new Promise((resolve)=>{
+	setTimeout(()=>{
+		resolve(images[1].url);
+	}.1000)
+})
+let file3 = new Promise((resolve)=>{
+	setTimeout(()=>{
+		resolve(images[2].url);
+	}.3000)
+})
+let i = 1;
+let x = Promise.all([file1,file2,file3]);
+x.then(images=>{
+	images.forEach((image)=>{
+		const a = document.createElement("a");
+        a.href = image;
+		a.setAttribut('download', `${i}`);
+		i++;
+		output.appenchChild(a);
+	})
+	
+})
 
-document.getElementById('download-images-button').addEventListener('click', downloadImages);
+
+
+
+
+

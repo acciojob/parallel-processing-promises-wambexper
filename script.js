ef461ac513b5780d02d1b6f61f0329f638d3e509
PETER WAMBEDDE <wambexpeter@gmail.com>
2023-11-24 17:33:05 +0300

    created a submission

--- script.js
+++ script.js
@@ -1,42 +1,49 @@
-// Sample array of image URLs
-const imageUrls = [
-    { url: 'url1.jpg' },
-    { url: 'url2.jpg' },
-    { url: 'url3.jpg' },
-    // Add more image URLs as needed
+//your JS code here. If required.
+const images = [
+  {
+    url: "https://picsum.photos/id/237/200/300",
+    alt: "Image 1",
+  },
+  {
+    url: "https://picsum.photos/id/238/200/300",
+    alt: "Image 2",
+  },
+  {
+    url: "https://picsum.photos/id/239/200/300",
+    alt: "Image 3",
+  }
+  
 ];
 
-// Function to download an image given its URL
-const downloadImage = (image) => {
+function downloadImages(images) {
+  const promises = images.map(image => {
     return new Promise((resolve, reject) => {
-        const img = new Image();
-        img.onload = () => resolve(img);
-        img.onerror = () => reject(`Failed to load image's URL: ${image.url}`);
-        img.src = image.url;
+      const img = new Image();
+      img.src = image.url;
+      img.alt = image.alt;
+      img.onload = () => {
+        resolve(img);
+      };
+      img.onerror = () => {
+        reject(`Failed to load image's URL: ${image.url}`);
+      };
     });
-};
+  });
 
-// Function to download all images in parallel using Promise.all
-const downloadAllImages = (imageUrls) => {
-    const promises = imageUrls.map(downloadImage);
-    return Promise.all(promises);
-};
-
-// Function to display the downloaded images in the output div
-const displayImages = (images) => {
-    const outputDiv = document.getElementById('output');
-    images.forEach((img) => {
-        outputDiv.appendChild(img);
+  Promise.all(promises)
+    .then(imgs => {
+      const output = document.getElementById('output');
+		output.innerHTML = null;
+      imgs.forEach(img => {
+        output.appendChild(img);
+      });
+    })
+    .catch(error => {
+      console.error(error);
     });
-};
+}
 
-// Event listener for the download button
-document.getElementById('download-images-button').addEventListener('click', () => {
-    downloadAllImages(imageUrls)
-        .then((images) => {
-            displayImages(images);
-        })
-        .catch((error) => {
-            console.error(error);
-        });
+const button = document.getElementById('download-images-button');
+button.addEventListener('click', () => {
+  downloadImages(images);
 });
